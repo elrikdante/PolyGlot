9f71f89f88ed7148c4e3c6b4212648a1b6de94e7
Dante Elrik <dante@cloudshopper.com>
2015-01-31 15:33:08 -0500

    A4 QuickUnion Fold

--- Src/Princeton/Main.hs
+++ Src/Princeton/Main.hs
@@ -1,11 +1,30 @@
 module Main
 (main)
+
 where
 
 import Princeton.A4
+import Data.List.Split(splitOn)
 
 main :: IO()
 main = do
-         putStrLn "Hello"
-         putStrLn "Yooo"
-         putStrLn "Hi"
+         d <- readFile "data.txt"
+
+         let
+           (size:unions) = lines d
+
+           sSize :: Int
+           sSize = read size
+
+           pairs :: [[Int]]
+           pairs = map (map read . (splitOn " ")) unions
+
+           qf    = initQF sSize 0
+           qf'   = foldl (\ qf (n:m:[]) -> union qf n m) qf pairs
+    
+         putStrLn $ "Size:" ++ (show sSize)
+         putStrLn $ show pairs
+         putStrLn $ show $ qf' 
+         putStrLn $ "Connected C:" ++ (show $  connectedComponents qf')
+         putStrLn (show (isConnected qf' 0 7))
+         putStrLn (show (isConnected qf' 8 9   ))
--- Src/Princeton/Princeton/A4.hs
+++ Src/Princeton/Princeton/A4.hs
@@ -3,7 +3,17 @@ module Princeton.A4
 (QuickFind (..),
 QuickUnion (..),
 Node (..),
-initQF)
+NodeID (..),
+RootID (..),
+NodeLength (..),
+initQF,
+union,
+isConnected,
+find,
+root,
+connectedComponents
+)
+
 
 where
 
@@ -17,51 +27,67 @@ type NodeLength = Int
 type RootID     = Int
 type NodeID     = Int
 
-initQF       :: Int -> a -> QuickFind a
+
+initQF :: Int  -> a  -> QuickFind a
+
 initQF n base = QuickFind nodes
   where
     rootID :: RootID
     rootID = 0
     nodes  = S.iterateN n (\(Root id v) -> Root (succ id) v) (Root rootID base)
 
-runQF
-  :: QuickFind a
-     -> S.Seq (Node a)
+runQF :: QuickFind a  -> S.Seq (Node a)
 runQF (QuickFind s) = s
 
-connectedComponents
-  :: QuickFind a
-  -> S.Seq (Node a)
-
+connectedComponents  :: QuickFind a  -> S.Seq (Node a)
 connectedComponents (QuickFind seq) = S.filter rootFilterFn seq
                                       where
                                         rootFilterFn node = case node of
                                                            Root _ _ -> True
-                                                           _        -> False                                                        
+                                                           _        -> False
 root
   :: QuickFind a
-     -> Node a
-     -> RootID
+  -> Node a
+  -> RootID
 
 root _ node@(Root id _)            = id
 root qf@(QuickFind a) (Node r v)   = let node = S.index a r in root qf node
 
+
+find
+  :: QuickFind a
+  -> NodeID
+  -> Node a
+find (QuickFind node_seq) id = S.index node_seq id
+
+isConnected
+  :: QuickFind a
+  -> NodeID
+  -> NodeID
+  -> Bool
+
+isConnected qf n m = root qf n' == root qf m'
+                     where
+                       n' = find qf n
+                       m' = find qf m
+
 union
   :: QuickFind a
-     -> NodeID
-     -> NodeID
-     -> QuickFind a
+  -> NodeID
+  -> NodeID
+  -> QuickFind a
 
 union qf@(QuickFind node_seq) n m = QuickFind node_seq'
                                      where
                                        n' = case (child) of
-                                     
+
                                          Root r v  -> case (r == (root qf parent)) of
                                            True  -> Root r v
                                            False -> Node m v
-                                       
+
                                          Node r a -> Node m a
-                                       
+
                                        node_seq' = S.update n n' node_seq
-                                       child     = S.index node_seq n
-                                       parent    = S.index node_seq m
+                                       child     = find qf n
+                                       parent    = find qf m
+
