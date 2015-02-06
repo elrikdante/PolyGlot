a4c78ee543517d277f9428bdc366e7a9c2e0ddc2
Dante Elrik <dante@cloudshopper.com>
2015-02-06 13:59:18 -0500

    Log parser working somewhat for NGINX logs

--- Src/Hacks/LogParser/src/LogParser/LogParser.hs
+++ Src/Hacks/LogParser/src/LogParser/LogParser.hs
@@ -4,18 +4,24 @@ module LogParser.LogParser
         Log(..),
         LogEntry(..),
         parseLog,
+        parseNginxLog,
         parseIP,
         parseTime,
+        parseNginxTime,
         parseProduct,
+        parseNginxLogEntry,
         parseLogEntry,
         productFromID,
+
         productToID)
        where
 
 import           Control.Applicative
-import           Data.Attoparsec.Char8
+import           Data.Attoparsec.Char8 as P
+import qualified Data.ByteString.Char8 as C8
 import qualified Data.Time as T
-import           Data.Word
+import           Data.Word as B
+
 
 -- Word8: 8-bit unsigned integer values
 data IP = IP Word8 Word8 Word8 Word8 deriving Show
@@ -25,6 +31,8 @@ data IP = IP Word8 Word8 Word8 Word8 deriving Show
 -- it also lets us map from an Int to a Product.
 data Product = Mouse | Keyboard | Monitor | Speakers deriving (Enum,Show)
 
+data HttpMethod = GET | POST | PUT | PATCH deriving Show
+
 productFromID :: Int -> Product
 productFromID = toEnum . (subtract 1)
 
@@ -32,11 +40,19 @@ productToID :: Product -> Int
 productToID = succ . fromEnum
 
 data LogEntry = LogEntry {
-  entryTime    :: T.LocalTime,
- entryIP      :: IP,
- entryProduct :: Product} deriving Show
+                      entryTime    :: T.LocalTime,
+                      entryIP      :: IP,
+                     entryProduct :: Product
+                }
+                | NginxLogEntry {
+                    entryTime :: T.LocalTime,
+                    challengeToken :: String,
+                    httpMethod :: HttpMethod
+                }
+              deriving Show
 
 type Log = [LogEntry]
+
 
 parseIP :: Parser IP
 parseIP = do
@@ -56,6 +72,39 @@ parseProduct =
   <|> (string "speakers" >> return Speakers)
   <|> (string "monitor" >> return Monitor)
 
+parseNginxTime :: Parser T.LocalTime
+parseNginxTime = do
+  dd <- count 2 digit
+  char '/'
+  mName <- P.takeWhile $ inClass "a-zA-Z"
+  char '/'
+  yyyy <- count 4 digit
+  char ':'
+  hh <- count 2 digit
+  char ':'
+  mm <- count 2 digit
+  char ':'
+  ss <- count 2 digit
+  return T.LocalTime { T.localDay = T.fromGregorian (read yyyy) (monthAbrToInt mName) (read dd),
+                         T.localTimeOfDay = T.TimeOfDay (read hh) (read mm) (read ss)
+                       }
+
+  where
+    monthAbrToInt :: C8.ByteString -> Int
+    monthAbrToInt "Jan" = 1
+    monthAbrToInt "Feb" = 2
+    monthAbrToInt "Mar" = 3
+    monthAbrToInt "Apr" = 4
+    monthAbrToInt "May" = 5
+    monthAbrToInt "Jun" = 6
+    monthAbrToInt "Jul" = 7
+    monthAbrToInt "Aug" = 8
+    monthAbrToInt "Sep" = 9
+    monthAbrToInt "Oct" = 10
+    monthAbrToInt "Nov" = 11
+    monthAbrToInt "Dec" = 12
+    monthAbrToInt a = error "Invalid MonthName"
+
 parseTime :: Parser T.LocalTime
 parseTime = do
   yyyy <- count 4 digit
@@ -72,7 +121,31 @@ parseTime = do
   return T.LocalTime { T.localDay  = T.fromGregorian (read yyyy) (read mm) (read dd),
                        T.localTimeOfDay = T.TimeOfDay (read hh) (read jj) (read ss)
                      }
-  
+
+
+parseHttpMethod :: Parser HttpMethod
+parseHttpMethod = do
+     (string "GET" >> return GET)
+  <|>(string "POST" >> return POST)
+
+parseNginxLogEntry :: Parser LogEntry
+parseNginxLogEntry = do
+  ip <- parseIP
+  string " - - ["
+  time <- parseNginxTime
+  string "+0000] \""
+  httpMethod <- parseHttpMethod
+  string " /guest/challenge/"
+  challengeToken <- takeTill ( ==' ')
+
+  return NginxLogEntry {entryTime = time,
+                        challengeToken = (C8.unpack challengeToken),
+                        httpMethod = httpMethod
+                       }
+
+parseNginxLog :: Parser Log
+parseNginxLog = many $ parseNginxLogEntry <* endOfLine
+
 parseLogEntry :: Parser LogEntry
 parseLogEntry = do
   time <- parseTime
--- Src/Hacks/LogParser/src/Main.hs
+++ Src/Hacks/LogParser/src/Main.hs
@@ -7,7 +7,8 @@ import qualified Data.ByteString as B
 import LogParser.LogParser
 
 main :: IO ()
-main =
-  do
-    d <- B.readFile "data.txt"
-    print $ parseOnly parseLog d
+main = do
+  let
+    str = "10.1.1.167 - - [29/Nov/2014:05:12:09 +0000] \"GET /guest/challenge/f43dd5e3-18b3-4363-beb7-469a29c538c8 HTTP/1.1\" 404 65 \"-\" \"AdsBot-Google (+http://www.google.com/adsbot.html)\" \"72.14.199.65\""
+  d <- B.readFile "data.txt"
+  print $ parseOnly parseNginxLogEntry "10.1.1.167 - - [29/Nov/2014:05:12:09+0000] \"GET /guest/challenge/f43dd5e3-18b3-4363-beb7-469a29c538c8 HTTP/1.1\"\n"
