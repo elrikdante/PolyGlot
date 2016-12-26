90202786e80c0d26f83610148a9a9bda60f1888f
Dante Haskell Elrik <elrikdante@users.noreply.github.com>
2016-12-26 00:44:12 -0500

    Create Endo-Difference-List.hs

---
+++ Src/RWH/Endo-Difference-List.hs
@@ -0,0 +1,30 @@
+import Data.Monoid
+
+newtype EDList a = EDL { unEDL :: Endo [a] }
+
+singleton :: a -> EDList a
+singleton = EDL . Endo . (:)
+
+empty :: EDList a
+empty  = EDL (Endo id)
+
+cons :: a -> EDList a -> EDList a
+cons x (EDL xs) = EDL (Endo (x:) <> xs)
+
+toList :: EDList a -> [a]
+toList = flip appEndo [] . unEDL
+
+fromList :: [a] -> EDList a
+fromList = Prelude.foldr go empty where go = cons
+
+append :: EDList a -> EDList a -> EDList a
+append (EDL xs) (EDL ys) = EDL (xs <> ys)
+
+foldr :: (a -> b -> b) -> b -> EDList a -> b
+foldr f z = Prelude.foldr f z . toList
+
+map  :: (a -> b) -> EDList a -> EDList b
+map f = Main.foldr go empty where go = cons . f
+
+listA = fromList [1..5]
+listB = fromList [10..20]
