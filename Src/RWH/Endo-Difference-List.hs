cb59b8fe5f1065cc1af89f91daec17d701bf7d2f
Dante Haskell Elrik <elrikdante@users.noreply.github.com>
2016-12-26 13:06:07 -0500

    add Monoid instance, replicate, and repeat.

--- Src/RWH/Endo-Difference-List.hs
+++ Src/RWH/Endo-Difference-List.hs
@@ -1,6 +1,11 @@
 import Data.Monoid
+import Data.Function
 
 newtype EDList a = EDL { unEDL :: Endo [a] }
+
+instance Monoid (EDList a) where
+  mempty = empty
+  mappend = append
 
 singleton :: a -> EDList a
 singleton = EDL . Endo . (:)
@@ -14,21 +19,25 @@ cons x (EDL xs) = EDL (Endo (x:) <> xs)
 toList :: EDList a -> [a]
 toList = flip appEndo [] . unEDL
 
-fromList :: [a] -> EDList a
-fromList = Prelude.foldr go empty where go = cons
+fromList,fromList' :: [a] -> EDList a
+fromList' = Prelude.foldr cons empty
+fromList  = EDL . Endo . (++)
 
 append :: EDList a -> EDList a -> EDList a
 append (EDL xs) (EDL ys) = EDL (xs <> ys)
 
-foldr :: (a -> b -> b) -> b -> EDList a -> b
+foldr     :: (a -> b -> b) -> b -> EDList a -> b
 foldr f z = Prelude.foldr f z . toList
 
 map  :: (a -> b) -> EDList a -> EDList b
-map f = Main.foldr go empty where go = cons . f
+map f = Main.foldr (cons . f) empty
 
-replicate :: a -> EDList a
-replicate = EDL . Endo . const . fix . (:)
+repeat :: a -> EDList a
+repeat = EDL . Endo . const . fix . (:)
 
+replicate 0 _ = EDL (Endo id)
+replicate n a = singleton a <> Main.replicate (n - 1) a
+            
 take :: Int -> EDList a -> EDList a
 take n = fromList . Prelude.take n . toList
 
